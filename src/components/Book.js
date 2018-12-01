7d6c2d41d9c03264f6a365955899a61aeea89800
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-12-01 09:35:20 +0530

    Fix issue where shelf value of book was not updated on the front end

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,40 +1,62 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 
-const Book = ({ book, updateBook }) => {
-  const { title, authors = [], imageLinks, shelf } = book
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 188,
-              backgroundImage: `url("${imageLinks && imageLinks.thumbnail}")`
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select
-              value={shelf || "none"}
-              onChange={e => updateBook(book, e.target.value)}
-            >
-              <option value="none" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+class Book extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: this.props.book.shelf || "none"
+    }
+    this.updateBook = this.updateBook.bind(this)
+  }
+
+  updateBook(value) {
+    const { book } = this.props
+    this.setState({ status: value })
+    this.props.updateBook(book, value)
+  }
+
+  render() {
+    const { book } = this.props
+    const { title, authors = [], imageLinks } = book
+    const { status } = this.state
+
+    const options = (
+      <React.Fragment>
+        <option disabled>Move to...</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </React.Fragment>
+    )
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 188,
+                backgroundImage: `url("${imageLinks && imageLinks.thumbnail}")`
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select
+                value={status}
+                onChange={e => this.updateBook(e.target.value)}
+              >
+                {options}
+              </select>
+            </div>
           </div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{authors[0] || "No author"}</div>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{authors[0] || "No author"}</div>
-      </div>
-    </li>
-  )
+      </li>
+    )
+  }
 }
 
 Book.propTypes = {
